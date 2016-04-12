3e02f3fa82c6cb6e5a8e78b64f191aef13ad8de2
Christian Amaral <darthcas@gmail.com>
2016-04-12 14:46:02 -0300

    f1rst

---
+++ index.js
@@ -0,0 +1,22 @@
+function createLocaleMiddleware (messages, defaultLocale) {
+  defaultLocale = defaultLocale || 'pt-BR'
+
+  /**
+   * reads locale headers and save it to `req.locale`
+   * @todo possibly read DEFAULT_LOCALE from domain country
+   * @param {Object} req express request
+   * @param {Object} res express response
+   * @param {Function} next next handler
+   * @returns {undefined}
+   */
+  function localeMiddleware (req, res, next) {
+    var cookieLocale = req.cookies[process.env.LOCALE_COOKIE_NAME]
+    var headerLocale = req.acceptsLanguages(Object.keys(messages))
+    var userLocale = req.user ? req.user.locale : null
+    req.locale = cookieLocale || headerLocale || userLocale || defaultLocale
+    next()
+  }
+  return localeMiddleware
+}
+
+module.exports = createLocaleMiddleware
